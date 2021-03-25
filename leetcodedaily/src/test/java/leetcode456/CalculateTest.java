7f0b4c139ba19f4e1ba8da15bf64e15f61549e92
zhuangpeng <zhuangpeng@asiainfo.com>
2021-03-25 11:30:15 +0800

    leetcode 第456  132模式 要求时间复杂度为O(n logn) 或 O(n)

---
+++ leetcodedaily/src/test/java/leetcode456/CalculateTest.java
@@ -0,0 +1,12 @@
+package leetcode456;
+
+import org.junit.Test;
+
+public class CalculateTest {
+
+    @Test
+    public void monotoneIncreasingDigits() {
+        Solution solution = new Solution();
+        System.out.println(solution.find132pattern(new int[]{3, 1, 2, 4}));
+    }
+}
