789a3f47816e887ef40c72467aae77869a6130aa
zhuangpeng <zhuangpeng@asiainfo.com>
2021-04-03 14:19:28 +0800

    leetcode 第13. 罗马数字转整数

---
+++ leetcodedaily/src/test/java/leetcode13/CalculateTest.java
@@ -0,0 +1,12 @@
+package leetcode13;
+
+import org.junit.Test;
+
+public class CalculateTest {
+
+    @Test
+    public void romanToInt() {
+        Solution solution = new Solution();
+        System.out.println(solution.romanToInt("III"));
+    }
+}
