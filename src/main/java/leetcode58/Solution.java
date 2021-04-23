12c6010e9c28ed193e399094140725fc198b62d7
zhuangpeng-wtt <wjnyjdc2020@)@)>
2021-04-23 09:50:43 +0800

    58. 最后一个单词的长度

---
+++ src/main/java/leetcode58/Solution.java
@@ -0,0 +1,21 @@
+package leetcode58;
+
+/**
+ * 58. 最后一个单词的长度
+ * 给你一个字符串 s，由若干单词组成，单词之间用空格隔开。返回字符串中最后一个单词的长度。如果不存在最后一个单词，请返回 0
+ * @author zhuangpeng
+ */
+public class Solution {
+
+    public int lengthOfLastWord(String s) {
+        int end = s.length() - 1;
+        while (end >= 0 && s.charAt(end) == ' ') {
+            end--;
+        }
+        int start = end;
+        while (start >= 0 && s.charAt(start) != ' ') {
+            start--;
+        }
+        return end - start;
+    }
+}
---
+++ src/test/java/leetcode58/CalculateTest.java
@@ -0,0 +1,12 @@
+package leetcode58;
+
+import org.junit.Test;
+
+public class CalculateTest {
+
+    @Test
+    public void lengthOfLastWord() {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLastWord("Hello World"));
+    }
+}
