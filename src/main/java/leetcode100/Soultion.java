bb8907e221143d8a8a55134c95cba08369f2e15e
zhuangpeng-wtt <wjnyjdc2020@)@)>
2021-04-26 20:29:08 +0800

    100. 相同的树

---
+++ src/main/java/leetcode100/Soultion.java
@@ -0,0 +1,26 @@
+package leetcode100;
+
+import dependency.TreeNode;
+
+/**
+ * 100. 相同的树
+ * 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ * @author zhuangpeng
+ */
+public class Soultion {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (null == p && null == q) {
+            return true;
+        }
+        if (null == p || null == q) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
