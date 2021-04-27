8d20a212e8a822f5ada6ca6505fd9a2fbe807b00
zhuangpeng-wtt <wjnyjdc2020@)@)>
2021-04-27 09:51:21 +0800

    938. 二叉搜索树的范围和 广度优先算法

--- src/main/java/leetcode938/Solution.java
+++ src/main/java/leetcode938/Solution.java
@@ -2,10 +2,20 @@ package leetcode938;
 
 import dependency.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author zhuangpeng
  */
 public class Solution {
+    /**
+     * dfs
+     * @param root
+     * @param low
+     * @param high
+     * @return
+     */
     public int rangeSumBST(TreeNode root, int low, int high) {
         if (null == root) {
             return 0;
@@ -21,4 +31,26 @@ public class Solution {
     }
 
 
+    public int rangeSumBST2(TreeNode root, int low, int high) {
+        int sum = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (null == node) {
+                continue;
+            } else if (node.val > high) {
+                q.offer(node.left);
+            } else if (node.val < low) {
+                q.offer(node.right);
+            } else {
+                sum += node.val;
+                q.offer(node.left);
+                q.offer(node.right);
+            }
+        }
+
+
+        return sum;
+    }
 }
