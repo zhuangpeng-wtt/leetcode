578c87db21a6e040e61a0017a7d39294c3a2f1e7
zhuangpeng-wtt <wjnyjdc2020@)@)>
2021-05-06 14:18:04 +0800

    1720. 解码异或后的数组

---
+++ src/main/java/leetcode1720/Solution.java
@@ -0,0 +1,16 @@
+package leetcode1720;
+
+/**
+ * @author zhuangpeng
+ */
+public class Solution {
+
+    public int[] decode(int[] encoded, int first) {
+        int[] ans = new int[encoded.length + 1];
+        ans[0] = first;
+        for (int i = 0; i < encoded.length; i++) {
+            ans[i + 1] = encoded[i] ^ ans[i];
+        }
+        return ans;
+    }
+}
