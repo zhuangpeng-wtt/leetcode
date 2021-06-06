6448e464c8fad4a23616ffffac590f2bcf243f1b
zhuangpeng-wtt <wjnyjdc2020@)@)>
2021-06-06 17:08:48 +0800

    1295. 统计位数为偶数的数字

---
+++ src/main/java/leetcode1295/Solution.java
@@ -0,0 +1,32 @@
+package leetcode1295;
+
+/**
+ * 1295. 统计位数为偶数的数字
+ * @author zhuangpeng
+ */
+public class Solution {
+    public int findNumbers(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {
+            if (((int)log(num, 10) + 1)%2 == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+
+    /**
+     * 根据换底公式使用java Math.log Aip 求对数公式
+     * @param basement 底数
+     * @param n 值
+     * @return
+     */
+    public static double log(int n, int basement){
+        return Math.log(n) / Math.log(basement);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(((int) (Math.log(1771) / Math.log(10)) + 1)%2);
+    }
+}
---
+++ src/test/java/leetcode1295/CalculateTest.java
@@ -0,0 +1,12 @@
+package leetcode1295;
+
+import org.junit.Test;
+
+public class CalculateTest {
+
+    @Test
+    public void findNumbers() {
+        Solution solution = new Solution();
+        System.out.println(solution.findNumbers(new int[]{555,901,482,1771}));
+    }
+}
